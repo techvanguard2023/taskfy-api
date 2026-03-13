427437a357242fe13ac1e3fd85037c48a01bad61
Cade Auto Peças <154639339+techvanguard2023@users.noreply.github.com>
2026-03-13 16:57:00 -0300

    ajusta rota

--- routes/api.php
+++ routes/api.php
@@ -10,26 +10,27 @@ use App\Http\Controllers\WpSettingController;
 Route::prefix('v1')->group(function () {
 
     Route::get('status', function () {
-        return response()->json(['status' => 'API V1 Taskfy is alive!'], 200);
-    });
+            return response()->json(['status' => 'API V1 Taskfy is alive!'], 200);
+        }
+        );
 
-    Route::post('login', [AuthController::class, 'login']);
-    Route::post('register', [AuthController::class, 'register']);
+        Route::post('login', [AuthController::class , 'login']);
+        Route::post('register', [AuthController::class , 'register']);
 
-    Route::middleware('auth:sanctum')->group(function () {
-        Route::post('/logout', [AuthController::class, 'logout']);
-        Route::get('/me', [AuthController::class, 'me']);
+        Route::middleware('auth:sanctum')->group(function () {
+            Route::post('/logout', [AuthController::class , 'logout']);
+            Route::get('/me', [AuthController::class , 'me']);
 
-        Route::get('users/phone/{phone}', [UserController::class, 'findByPhone']);
-        
-        Route::middleware('role:admin')->group(function () {
-            Route::apiResource('users', UserController::class);
+            Route::middleware('role:admin')->group(function () {
+                    Route::get('users/phone/{phone}', [UserController::class , 'findByPhone']);
+                    Route::apiResource('users', UserController::class);
+                }
+                );
+
+                Route::apiResource('tasks', TaskController::class);
+                Route::delete('wp-settings/name/{name}', [WpSettingController::class , 'destroyByName']);
+                Route::patch('wp-settings/name/{name}', [WpSettingController::class , 'updateByName']);
+                Route::apiResource('wp-settings', WpSettingController::class);
+            }
+            );
         });
-
-        Route::apiResource('tasks', TaskController::class);
-        Route::delete('wp-settings/name/{name}', [WpSettingController::class, 'destroyByName']);
-        Route::patch('wp-settings/name/{name}', [WpSettingController::class, 'updateByName']);
-        Route::apiResource('wp-settings', WpSettingController::class);
-    });
-
-});
